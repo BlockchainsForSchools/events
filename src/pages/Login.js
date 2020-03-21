ad06199cbd263cdb4122503c9662aebe7b86f490
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-21 15:48:32 -0400

    Create login and signup forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,87 @@
 // @flow
-import React from "react";
+import React from 'react';
+import {
+	Button,
+	CssBaseline,
+	TextField,
+	Link,
+	Grid,
+	Typography,
+	Container
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 type PropTypes = {};
 
+const useStyles = makeStyles(theme => ({
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(1)
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2)
+	}
+}));
+
 export const Login = (props: PropTypes) => {
+	const classes = useStyles();
+
 	return (
-		<div>
-		</div>
-	)
+		<Container component='main' maxWidth='xs'>
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Typography component='h1' variant='h4'>
+					Log In
+				</Typography>
+				<form className={classes.form} noValidate>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						id='email'
+						label='Email Address'
+						name='email'
+						autoComplete='email'
+						autoFocus
+					/>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						name='password'
+						label='Password'
+						type='password'
+						id='password'
+						autoComplete='current-password'
+					/>
+					<Button
+						type='submit'
+						fullWidth
+						variant='contained'
+						color='primary'
+						className={classes.submit}
+					>
+						Log In
+					</Button>
+					<Grid container justify='center'>
+						<Grid item>
+							<Link href='/sign-up' variant='body2'>
+								{"Don't have an account? Sign Up"}
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+		</Container>
+	);
 };
 
 export default Login;
