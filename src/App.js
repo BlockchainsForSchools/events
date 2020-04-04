f17cda395bc9ec1e3c345f4210fc8531aae0e42a
Kazi Jamal <jamalkazi02@gmail.com>
2020-04-04 14:39:40 -0400

    Fix Links and working ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
-import {AppProvider} from "./comps/AppProvider";
+import React from "react";
+import "./App.css";
+import { AppProvider } from "./comps/AppProvider";
 import Content from "./comps/Content";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#4CBB85",
+        },
+    },
+    status: {
+        danger: "orange",
+    },
+});
 
 function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<AppProvider bypass>
-					<Content/>
-				</AppProvider>
-			</BrowserRouter>
-		</div>
-	);
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <AppProvider bypass>
+                    <ThemeProvider theme={theme}>
+                        <Content />
+                    </ThemeProvider>
+                </AppProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,104 +1,109 @@
 // @flow
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import {
-	Button,
-	CssBaseline,
-	TextField,
-	Link,
-	Grid,
-	Typography,
-	Container
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import backend from '../utils/backend';
+    Button,
+    CssBaseline,
+    TextField,
+    Grid,
+    Typography,
+    Container,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import backend from "../utils/backend";
 
 type PropTypes = {};
 
-const useStyles = makeStyles(theme => ({
-	paper: {
-		marginTop: theme.spacing(8),
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center'
-	},
-	form: {
-		width: '100%',
-		marginTop: theme.spacing(1)
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2)
-	}
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    form: {
+        width: "100%",
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
-const handleLogin = event => {
-	event.preventDefault();
-	const email = event.target.email.value;
-	const password = event.target.password.value;
-	console.log('email: ' + email);
-	console.log('password: ' + password);
-	backend
-		.post('/api/auth/login', {
-			email: email,
-			password: password
-		})
-		.then(response => {
-			console.log(response);
-		});
+const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log("email: " + email);
+    console.log("password: " + password);
+    backend
+        .post("/api/auth/login", {
+            email: email,
+            password: password,
+        })
+        .then((response) => {
+            console.log(response);
+        });
 };
 
 export const Login = (props: PropTypes) => {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	return (
-		<Container component='main' maxWidth='xs'>
-			<CssBaseline />
-			<div className={classes.paper}>
-				<Typography component='h1' variant='h4'>
-					Log In
-				</Typography>
-				<form onSubmit={handleLogin} className={classes.form} noValidate>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						id='email'
-						label='Email Address'
-						name='email'
-						autoComplete='email'
-						autoFocus
-					/>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						name='password'
-						label='Password'
-						type='password'
-						id='password'
-						autoComplete='current-password'
-					/>
-					<Button
-						type='submit'
-						fullWidth
-						variant='contained'
-						color='primary'
-						className={classes.submit}
-					>
-						Log In
-					</Button>
-					<Grid container justify='center'>
-						<Grid item>
-							<Link to='/sign-up' variant='body2'>
-								{"Don't have an account? Sign Up"}
-							</Link>
-						</Grid>
-					</Grid>
-				</form>
-			</div>
-		</Container>
-	);
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h4">
+                    Log In
+                </Typography>
+                <form
+                    onSubmit={handleLogin}
+                    className={classes.form}
+                    noValidate
+                >
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Log In
+                    </Button>
+                    <Grid container justify="center">
+                        <Grid item>
+                            <Link to="/sign-up" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
+    );
 };
 
 export default Login;
