13a62a99421b8dad1738d4d50c0ed9eff5327e3b
ray. lee <rlee00@stuy.edu>
2020-04-25 21:59:52 -0400

    Form tests

--- src/pages/Auth/Login/Login.test.js
+++ src/pages/Auth/Login/Login.test.js
@@ -1,4 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+
+import { Login } from './Login';
+
+test('Signup form is submittable', async () => {
+  const { container } = render(<Login />);
+  const email = container.querySelector('input[name="email"]');
+  const password = container.querySelector('input[name="password"]');
+  const submit = container.querySelector('button[type="submit"]');
+  const results = container.querySelector("makestyles-form-2");
+
+  await wait(() => {
+    fireEvent.change(email, {
+      target: {
+        value: "mock@email.com"
+      }
+    })
+  })
+
+  await wait(() => {
+    fireEvent.change(password, {
+      target: {
+        value: "password"
+      }
+    })
+  })
+
+  await wait(() => {
+      fireEvent.click(submit)
+    })
+
+  expect(results.innerHTML).toBe(
+    '{"email":"mock@email.com","name":"mockname","color":"green"}'
+  )
+});
 
--- src/pages/Auth/SignUp/SignUp.test.js
+++ src/pages/Auth/SignUp/SignUp.test.js
@@ -1,4 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait, fireEvent } from '@testing-library/react';
+import ReactTestUtils from 'react-dom/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 
+import { SignUp } from './SignUp';
+
+test('Signup form is submittable', async () => {
+  const { container } = render(<SignUp />);
+  const firstName = container.querySelector('input[name="firstname"]');
+  const lastName = container.querySelector('input[name="lastname"]');
+  const password = container.querySelector('input[name="password"]');
+  const confirmPassword = container.querySelector('input[name="confirmPassword"]');
+  const results = container.querySelector("makestyles-form-2");
+
+  await wait(() => {
+    fireEvent.change(name, {
+      target: {
+        value: 'mockname',
+      },
+    });
+  });
+
+  await wait(() => {
+    fireEvent.change(email, {
+      target: {
+        value: 'mock@email.com',
+      },
+    });
+  });
+
+  await wait(() => {
+    fireEvent.click(submit);
+  });
+
+  expect(results.innerHTML).toBe(
+    '{"email":"mock@email.com","name":"mockname","color":"green"}'
+  );
+});
