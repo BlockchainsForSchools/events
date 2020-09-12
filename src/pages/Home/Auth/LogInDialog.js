00ea91c7d08037ab12abed04e8a1d104caba8b39
Kazi Jamal <jamalkazi02@gmail.com>
2020-09-12 18:28:43 -0400

    Create LogInDialog and SignUpDialog components

---
+++ src/pages/Home/Auth/LogInDialog.js
@@ -0,0 +1,114 @@
+// @flow
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { GoogleLogin } from "react-google-login";
+import { makeStyles } from "@material-ui/core/styles";
+
+import authserver from "../../../utils/authserver";
+
+type PropTypes = {};
+
+const useStyles = makeStyles((theme) => ({
+	loginForm: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center"
+	},
+}));
+
+const handleLogin = (event) => {
+	event.preventDefault();
+	const email = event.target.email.value;
+	const password = event.target.password.value;
+	console.log("email: " + email);
+	console.log("password: " + password);
+	// backend
+	// 	.post("/api/auth/login", {
+	// 		email: email,
+	// 		password: password
+	// 	})
+	// 	.then((response) => {
+	// 		console.log(response);
+	// 	});
+};
+
+const successResponseGoogle = (response) => {
+	var id_token = response.getAuthResponse().id_token;
+	console.log(id_token);
+	// backend
+	// 	.post("/api/auth/oauthlogin", {
+	// 		token: id_token
+	// 	})
+	// 	.then((response) => {
+	// 		console.log(response);
+	// 	});
+};
+
+const failureResponseGoogle = (response) => {
+	console.log("error: " + response.error);
+	console.log("details: " + response.details);
+};
+
+const LogInDialog = (props: PropTypes) => {
+	const classes = useStyles();
+	const { open, handleClose } = props;
+
+	return (
+		<Dialog
+			open={open}
+			onClose={handleClose}
+			aria-labelledby="form-dialog-title"
+		>
+			<DialogTitle id="form-dialog-title">Log In</DialogTitle>
+			<DialogContent className={classes.loginForm}>
+				<GoogleLogin
+					clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+					buttonText="Continue with Google"
+					onSuccess={successResponseGoogle}
+					onFailure={failureResponseGoogle}
+					cookiePolicy={"single_host_origin"}
+				/>
+                <br />
+				<form onSubmit={handleLogin} noValidate>
+					<TextField
+						variant="outlined"
+						margin="dense"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<DialogActions>
+						<Button onClick={handleClose} color="primary">
+							Cancel
+						</Button>
+						<Button type="submit" color="primary">
+							Log In
+						</Button>
+					</DialogActions>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default LogInDialog;
---
+++ src/pages/Home/Auth/SignUpDialog.js
@@ -0,0 +1,121 @@
+// @flow
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { makeStyles } from "@material-ui/core/styles";
+
+// import authserver from "../../../utils/authserver";
+
+type PropTypes = {};
+
+const useStyles = makeStyles((theme) => ({
+	signUpForm: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center"
+	}
+}));
+
+const handleSignUp = (event) => {
+	event.preventDefault();
+	const firstName = event.target.firstName.value;
+	const lastName = event.target.lastName.value;
+	const email = event.target.email.value;
+	const password = event.target.password.value;
+	const password2 = event.target.password2.value;
+	console.log("first name: " + firstName);
+	console.log("last name: " + lastName);
+	console.log("email: " + email);
+	console.log("password: " + password);
+	console.log("confirm password: " + password2);
+};
+
+const SignUpDialog = (props: PropTypes) => {
+	const classes = useStyles();
+	const { open, handleClose } = props;
+
+	return (
+		<Dialog
+			open={open}
+			onClose={handleClose}
+			aria-labelledby="form-dialog-title"
+		>
+			<DialogTitle id="form-dialog-title">Sign Up</DialogTitle>
+			<DialogContent className={classes.signUpForm}>
+				<form onSubmit={handleSignUp} noValidate>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete="fname"
+								name="firstName"
+								variant="outlined"
+								required
+								fullWidth
+								id="firstName"
+								label="First Name"
+								autoFocus
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="lastName"
+								label="Last Name"
+								name="lastName"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant="outlined"
+								required
+								fullWidth
+								name="password2"
+								label="Confirm Password"
+								type="password"
+								id="password2"
+							/>
+						</Grid>
+					</Grid>
+					<DialogActions>
+						<Button onClick={handleClose} color="primary">
+							Cancel
+						</Button>
+						<Button type="submit" color="primary">
+							Sign Up
+						</Button>
+					</DialogActions>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default SignUpDialog;
